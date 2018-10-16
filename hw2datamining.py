f3dc507fb6198e5e281b745b44eaf9d176b2162d
empireisme <empireisme@gmail.com>
2018-10-16 20:35:47 +0800

    123

--- hw2datamining.py
+++ hw2datamining.py
@@ -9,5 +9,5 @@ import pandas as pd
 from pandas import Series,DataFrame
 df=pd.read_excel('C:\\Users\\User\\Documents\\GitHub\\L4Python\\hw2data.xls') 
 
-df1=df.iloc[  4914:6569 ,:   ] #抽出了10月份到12月份的資料 但是使用目測法
+df1=df.iloc[  4914:6569 ,:   ] #抽出了10月份到12月份的資料 但是使用目測法 123wFASDASDFADFA
 
